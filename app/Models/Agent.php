5ee2dd8fa24e7904b760a95e47071247b72d1832
Favour Okunowo <43044740+NowoCodes@users.noreply.github.com>
2021-04-10 04:09:29 +0100

    add student & agent Models

---
+++ app/Models/Agent.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+
+class Agent extends Authenticatable
+{
+    use HasFactory, Notifiable;
+
+    protected $guard = 'agent';
+
+    protected $fillable = [
+        'name', 'email', 'phone', 'image', 'password',
+    ];
+
+    protected $hidden = [
+        'password',
+    ];
+}
---
+++ app/Models/Student.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+
+class Student extends Authenticatable
+{
+    use HasFactory, Notifiable;
+
+    protected $guard = 'student';
+
+    protected $fillable = [
+        'name', 'email', 'phone', 'location', 'password',
+    ];
+
+    protected $hidden = [
+        'password',
+    ];
+}
