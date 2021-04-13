6a7f3218edadd59c90241248b27100809f7cd03c
Favour Okunowo <43044740+NowoCodes@users.noreply.github.com>
2021-04-13 13:32:45 +0100

    configure login authentication for all users in one file

---
+++ app/Http/Controllers/Auth/AuthController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Providers\RouteServiceProvider;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class AuthController extends Controller
+{
+    public function create()
+    {
+        return view('auth.login');
+    }
+
+    public function store(Request $request)
+    {
+        // Validate Fields before Logging In
+        $request->validate([
+            'email' => 'required|email|max:255',
+            'password' => 'required|min:6',
+        ]);
+
+        // Log in based on type of user
+        $credentials = $request->only('email', 'password');
+
+        // if admin
+        if (Auth::attempt($credentials)) {
+            $request->session()->regenerate();
+            return redirect()->intended(RouteServiceProvider::ADMINHOME);
+        } else if (Auth::guard('student')->attempt($credentials)) {
+            // if student
+            $request->session()->regenerate();
+            return redirect()->intended(RouteServiceProvider::STUDENTHOME)
+                ->with('status', 'You\'re now logged in.' );
+        } else if (Auth::guard('agent')->attempt($credentials)) {
+            // if agent
+            $request->session()->regenerate();
+            return redirect()->intended(RouteServiceProvider::AGENTHOME)
+                ->with('status', 'You\'re now logged in.' );
+        } else {
+            return back()->withErrors([
+                'email' => 'The provided credentials do not match our records.',
+            ]);
+        }
+    }
+
+    public function destroy(Request $request)
+    {
+        // specific user logout
+        if (Auth::logout()) {
+            $request->session()->invalidate();
+            $request->session()->regenerateToken();
+        } else if (Auth::guard('student')->logout()) {
+            $request->session()->invalidate();
+            $request->session()->regenerateToken();
+        } else if (Auth::guard('agent')->logout()) {
+            $request->session()->invalidate();
+            $request->session()->regenerateToken();
+        }
+
+        return redirect(route('onboard.login'));
+    }
+}
