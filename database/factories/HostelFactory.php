de75dc26fc54f551fc6ff0cf53441ee2e369b2ea
Favour Okunowo <43044740+NowoCodes@users.noreply.github.com>
2021-04-23 22:44:00 +0100

    seed db

---
+++ database/factories/HostelFactory.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Hostel;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class HostelFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Hostel::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'agent_id' => $this->faker->numberBetween(1,2),
+            'hostel_name' => $this->faker->word,
+            'state' => $this->faker->state,
+            'town' => $this->faker->city,
+            'address' => $this->faker->address,
+            'property' => $this->faker->randomElement(['Flats', 'Detached', 'Duplex', 'Bungalow']),
+            'roomNum' => $this->faker->randomDigit,
+            'amount' => $this->faker->numerify('#####'),
+            'period' => $this->faker->randomElement(['Yearly', 'Monthly', 'Quaterly']),
+            'tenantType' => $this->faker->word,
+            'image_name' => $this->faker->imageUrl(),
+        ];
+    }
+}
