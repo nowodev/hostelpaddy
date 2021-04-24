676a300063e3e21282ab63c3c293109a1cc47098
Favour Okunowo <43044740+NowoCodes@users.noreply.github.com>
2021-04-24 16:25:47 +0100

    create test routes

--- routes/custom/dashboard.php
+++ routes/custom/dashboard.php
@@ -1,6 +1,9 @@
 <?php
 
 use App\Http\Controllers\HostelsListingController;
+use App\Models\Agent;
+use App\Models\Hostel;
+use App\Models\Utility;
 use Illuminate\Support\Facades\Route;
 
 
@@ -20,5 +23,21 @@ Route::middleware(['auth:agent'])->group(function () {
     Route::get('/agent', function () {
         return view('agents.index');
     })->name('agent.index');
+
     Route::resource('listings', HostelsListingController::class);
+});
+
+Route::get('/test', function () {
+    // return Hostel::with(['amenities', 'utilities', 'rules'])
+    //         ->orderBy('id')->get();
+
+    return Hostel::with(['amenities', 'utilities', 'rules'])
+        ->where('id', 50)->first();
+});
+
+Route::get('/img', function()
+{
+    $imagePath = public_path('storage/10000680.jpg');
+    $img = Image::make($imagePath)->resize(300, 200);
+    return $img->response('jpg');
 });
