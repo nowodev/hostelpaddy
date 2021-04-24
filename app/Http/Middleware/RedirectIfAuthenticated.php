4c863a9e10a41e9f26a222917b4915b76cec8d2f
Favour Okunowo <43044740+NowoCodes@users.noreply.github.com>
2021-04-24 16:25:29 +0100

    Update RedirectIfAuthenticated.php

--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -22,7 +22,7 @@ class RedirectIfAuthenticated
 
         foreach ($guards as $guard) {
             if (Auth::guard($guard)->check()) {
-                return redirect(RouteServiceProvider::HOME);
+                return redirect(RouteServiceProvider::ADMINHOME);
             }
 
             if ($guard == 'student' && Auth::guard($guard)->check()) {
