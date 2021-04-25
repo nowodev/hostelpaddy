2d713bb1c0685c6389fec02fce2bc9b75d593181
Favour Okunowo <43044740+NowoCodes@users.noreply.github.com>
2021-04-25 20:21:02 +0100

    show links on small screen

--- resources/views/layouts/agents/navigation.blade.php
+++ resources/views/layouts/agents/navigation.blade.php
@@ -97,10 +97,19 @@
   <!-- Responsive Navigation Menu -->
   <div :class="{'block': open, 'hidden': ! open}" class="hidden sm:hidden">
     <div class="pt-2 pb-3 space-y-1">
-      <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
-        {{ __('Dashboard') }}
+      <x-responsive-nav-link>
+        {{ __('Hostel') }}
+      </x-responsive-nav-link>
+
+      <x-responsive-nav-link :href="route('listings.index')" :active="request()->routeIs('listings.index')">
+        {{ __('Manage Listing') }}
+      </x-responsive-nav-link>
+
+      <x-responsive-nav-link>
+        {{ __('Help') }}
       </x-responsive-nav-link>
     </div>
+
 
     <!-- Responsive Settings Options -->
     <div class="pt-4 pb-1 border-t border-gray-200">
