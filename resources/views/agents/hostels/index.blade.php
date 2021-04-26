dcef9a8440d7d1aadff9cab3a45fdbe162865e2e
Favour Okunowo <43044740+NowoCodes@users.noreply.github.com>
2021-04-26 03:55:09 +0100

    Update index.blade.php

--- resources/views/agents/hostels/index.blade.php
+++ resources/views/agents/hostels/index.blade.php
@@ -35,7 +35,7 @@
                 <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
                   Type
                 </th>
-                <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                <th scope="col" class="py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
                   Price
                 </th>
                 <th scope="col" class="relative px-6 py-3">
@@ -70,10 +70,9 @@
                   <td class="px-6 py-4 whitespace-nowrap">
                     <div class="text-sm text-gray-900">{{ $hostel->property }} with {{ $hostel->roomNum }} {{ $hostel->roomNum < 2 ? 'room' : 'rooms' }}
                     </div>
-                    <div class="text-sm text-gray-500">...</div>
                   </td>
-                  <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500 font-bold">
-                    N{{ $hostel->amount }}
+                  <td class="py-4 whitespace-nowrap text-sm text-gray-700">
+                    N{{ $hostel->amount }} {{ $hostel->period }}
                   </td>
                   <td class="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
                     <a href="{{ route('listings.show', [$hostel]) }}"
