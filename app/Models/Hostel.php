79d27b0683d732d027236c99bf8ce64d5450a67b
Favour Okunowo <toluwaniokunowo@gmail.com>
2021-04-30 10:51:54 +0100

    define relationships & use softdeletes

--- app/Models/Hostel.php
+++ app/Models/Hostel.php
@@ -4,8 +4,45 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Hostel extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'hostel_name', 'state', 'city',
+        'address', 'property', 'roomNum',
+        'amount', 'period', 'tenantType', 'image_name',
+        'amenities', 'utilities', 'rules',
+    ];
+
+    public function agents()
+    {
+        return $this->belongsTo(Agent::class);
+    }
+
+    public function amenities()
+    {
+        return $this->belongsToMany(Amenity::class);
+    }
+
+    public function utilities()
+    {
+        return $this->belongsToMany(Utility::class);
+    }
+
+    public function rules()
+    {
+        return $this->belongsToMany(Rule::class);
+    }
+
+    // display image
+    public function getThumbnailAttribute()
+    {
+        if ($this->image) {
+            return asset('storage/hostels/' . $this->image);
+        }
+        return asset('storage/thumbnail.jpg');
+    }
 }
