8ce765afe3ee182cf83c8f5da7f0c3f11673a1cb
Favour Okunowo <toluwaniokunowo@gmail.com>
2021-04-30 10:52:36 +0100

    change resource controller name

--- app/Http/Controllers/Agent/HostelController.php
+++ app/Http/Controllers/Agent/HostelController.php
@@ -3,84 +3,113 @@
 namespace App\Http\Controllers\Agent;
 
 use App\Models\Hostel;
-use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Http\Requests\HostelRequest;
+use App\Models\Amenity;
+use App\Models\Property;
+use App\Models\Rule;
+use App\Models\Utility;
+use Intervention\Image\ImageManagerStatic as Image;
 
 class HostelController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
+    public function __construct()
+    {
+        $this->authorizeResource(Hostel::class, 'hostel');
+    }
+
     public function index()
     {
-        //
+        // ? fetch hostels from table
+        $hostels = Hostel::where('agent_id', auth('agent')->id())
+            ->orderBy('id', 'ASC')
+            ->Paginate(10);
+        return view('agents.hostels.index', compact('hostels'));
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function create()
     {
-        //
+        // ? get Models, to insert them as arrays
+        $hostel = new Hostel();
+        $amenities = Amenity::get();
+        $utilities = Utility::get();
+        $rules = Rule::get();
+        $properties = Property::get();
+
+        return view('agents.hostels.create',
+            compact('hostel', 'amenities', 'utilities', 'rules', 'properties')
+        );
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
+    public function store(HostelRequest $request)
     {
-        //
+        // ? Add hostel to table, and populate other tables(hostel_utility, amenity_hostel, hostel_rule) with data
+        if (auth('agent')->check()) {
+            $hostel = auth('agent')->user()->hostels()->create($request->validated());
+            if ($request->hasFile('image')) {
+                $this->_uploadImage($request, $hostel);
+            }
+
+            $hostel->amenities()->sync($request->amenities);
+            $hostel->utilities()->sync($request->utilities);
+            $hostel->rules()->sync($request->rules);
+            return redirect()->route('hostels.index')
+                ->with('success', 'Hostel Added Successfully');
+        } else {
+            return redirect()->back()
+                ->with('error', 'A problem occurred');
+        }
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Models\Hostel  $hostel
-     * @return \Illuminate\Http\Response
-     */
     public function show(Hostel $hostel)
     {
-        //
+        return view('agents.hostels.show', compact('hostel'));
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Models\Hostel  $hostel
-     * @return \Illuminate\Http\Response
-     */
     public function edit(Hostel $hostel)
     {
-        //
+        $amenities = Amenity::get();
+        $utilities = Utility::get();
+        $rules = Rule::get();
+        $properties = Property::get();
+
+        return view('agents.hostels.edit',
+            compact('hostel', 'amenities', 'utilities', 'rules', 'properties')
+        );
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\Hostel  $hostel
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, Hostel $hostel)
+    public function update(HostelRequest $request, Hostel $hostel)
     {
-        //
+        $hostel->update($request->validated());
+        if ($request->hasFile('image')) {
+            $this->_uploadImage($request, $hostel);
+        }
+
+        $hostel->amenities()->sync($request->amenities);
+        $hostel->utilities()->sync($request->utilities);
+        $hostel->rules()->sync($request->rules);
+
+        return redirect()->route('hostels.index')
+            ->with('success', 'Hostel Updated Successfully');
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Models\Hostel  $hostel
-     * @return \Illuminate\Http\Response
-     */
     public function destroy(Hostel $hostel)
     {
-        //
+        $hostel->amenities()->detach($hostel->amenities);
+        $hostel->utilities()->detach($hostel->utilities);
+        $hostel->rules()->detach($hostel->rules);
+        $hostel->delete();
+        return redirect()->route('hostels.index')
+            ->with('success', 'Hostel Deleted Successfully');
+    }
+
+    private function _uploadImage($request, $hostel)
+    {
+        $image = $request->file('image');
+        $filename = 'HP_H_' . time() . '.' . $image->getClientOriginalExtension();
+        Image::make($image)->resize(225, 100)
+            ->save(storage_path('app/public/hostels/' . $filename));
+        $hostel->image = $filename;
+        $hostel->save();
     }
 }
--- routes/custom/dashboard.php
+++ routes/custom/dashboard.php
@@ -1,9 +1,6 @@
 <?php
 
-use App\Http\Controllers\HostelsListingController;
-use App\Models\Agent;
-use App\Models\Hostel;
-use App\Models\Utility;
+use App\Http\Controllers\Agent\HostelController;
 use Illuminate\Support\Facades\Route;
 
 
@@ -24,5 +21,5 @@ Route::middleware(['auth:agent'])->prefix('agent')->group(function () {
         return view('agents.index');
     })->name('agent.index');
 
-    Route::resource('listings', HostelsListingController::class);
+    Route::resource('hostels', HostelController::class);
 });
