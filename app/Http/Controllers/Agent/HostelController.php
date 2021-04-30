dfae1e9d281810581d5072db20be9b18e57cbdc8
Favour Okunowo <toluwaniokunowo@gmail.com>
2021-04-30 09:25:17 +0100

    Generate new test:model

---
+++ app/Http/Controllers/Agent/HostelController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers\Agent;
+
+use App\Models\Hostel;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class HostelController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Hostel  $hostel
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Hostel $hostel)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Hostel  $hostel
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Hostel $hostel)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Hostel  $hostel
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Hostel $hostel)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Hostel  $hostel
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Hostel $hostel)
+    {
+        //
+    }
+}
