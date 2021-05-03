278b716bb6def1931584a6ff3596c7cea656ac76
Favour Okunowo <toluwaniokunowo@gmail.com>
2021-05-03 01:58:30 +0100

    show availability dropdown only in edit form

--- resources/views/agents/hostels/edit.blade.php
+++ resources/views/agents/hostels/edit.blade.php
@@ -22,7 +22,7 @@
       @csrf
       @method('PUT')
 
-      @include('agents.hostels.form')
+      @include('agents.hostels.form', ['available' => true])
 
       <button type="submit"
         class="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-blue-600 hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 mt-3">
