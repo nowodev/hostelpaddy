b24a6ff85cff14d77fc02fb7e09a48b259c811f9
Favour Okunowo <toluwaniokunowo@gmail.com>
2021-06-23 13:15:42 +0100

    fix: contact page responsiveness

--- resources/views/frontend/contact.blade.php
+++ resources/views/frontend/contact.blade.php
@@ -16,8 +16,8 @@
       <div class="col-lg-6 col-md-6">
         <img src="{{ asset('main/contact-img/vpMw_IYoAu0 (1).png') }}" class="img-fluid" alt="" />
       </div>
-      <div class="col-lg-6 col-md-6">
-        <p>Send us your enquiries and we will get back to you</p>
+      <div class="col-md-6 col-lg-6">
+        <p class="mt-sm-3">Send us your enquiries and we will get back to you</p>
         <form class="enquires-form">
           <div class="form-group">
             <input type="text" class="form-control" placeholder="Enter Name" />
@@ -41,7 +41,8 @@
       <div class="col-12">
         <h3>Email <a href="#">hostelpaddy@gmail.com</a></h3>
         <h3>
-          Socials <a href="">
+          Socials
+          <a href="">
             <img src="{{ asset('main/img/instagram.png') }}" class="img-fluid mr-3" alt="" />
           </a>
           <a href="">
