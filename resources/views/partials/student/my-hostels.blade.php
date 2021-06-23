562340cf897642f7b1d0c6666f9664bc73529994
Favour Okunowo <toluwaniokunowo@gmail.com>
2021-06-23 01:18:34 +0100

    finish my hostels tab

---
+++ resources/views/partials/student/my-hostels.blade.php
@@ -0,0 +1,19 @@
+{{-- My Hostel Details --}}
+
+<div class="row mb-3">
+    <div class="col-4">
+      <div class="card">
+        <div class="row no-gutters">
+          <img class="img-fluid" src="{{ asset('main/img/hostel.png') }}" alt="">
+          <div class="col">
+            <span class="card-body fs-6">
+              <p class="card-text">1 Room Duplex Hostel</p>
+              <p>Bedroom, wardrobe, bathroom...</p>
+              <p>Surlere Lagos.</p>
+              <p class="">#5,000/monthly</p>
+            </span>
+          </div>
+        </div>
+      </div>
+    </div>
+</div>
--- resources/views/students/index.blade.php
+++ resources/views/students/index.blade.php
@@ -40,44 +40,16 @@
       </ul>
     </section>
 
-    {{-- Search bar --}}
-    <section class="mb-5">
-      <div class="container">
-        <div class="row pt-5">
-          <div class="col-sm-8">
-            <form action="#">
-              <input class="form-control" type="text" placeholder="  Seach location apartment..." />
-              {{-- <i class="fa fa-search" aria-hidden="true"></i> --}}
-            </form>
-          </div>
-          <div class="col-sm-2">
-            <span class="btn btn-outline-dark">Filter</span>
-          </div>
-          <div class="col-sm-2">
-            <span class="dropdown">
-              <button class="btn btn-outline-dark dropdown-toggle position-relative" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                Sort
-              </button>
-              <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-                <a class="dropdown-item" href="#">Action</a>
-                <a class="dropdown-item" href="#">Another action</a>
-                <a class="dropdown-item" href="#">Something else here</a>
-              </div>
-            </span>
-          </div>
-        </div>
-      </div>
-    </section>
-
     {{-- Tab Panes --}}
     <section class="tab-content">
-      <div id="shared-hostels" class="container tab-pane active"><br>
+      <div id="shared-hostels" class="container tab-pane active">
+        @include('partials.student.search-bar')
         @include('partials.student.shared-hostels')
       </div>
 
-      <div id="my-hostels" class="container tab-pane fade"><br>
-        <h3>Menu 1</h3>
-        <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+      <div id="my-hostels" class="container tab-pane fade">
+        @include('partials.student.share-hostel-search-bar')
+        @include('partials.student.my-hostels')
       </div>
       <div id="saved" class="container tab-pane fade"><br>
         <h3>Menu 2</h3>
