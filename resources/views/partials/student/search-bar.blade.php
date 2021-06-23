c8fc73860945cfc7c97482d5040c220c15a048d5
Favour Okunowo <toluwaniokunowo@gmail.com>
2021-06-23 01:16:38 +0100

    seperate search bars

---
+++ resources/views/partials/student/search-bar.blade.php
@@ -0,0 +1,29 @@
+{{-- Search bar --}}
+<section class="mb-5">
+  <div class="container">
+    <div class="row pt-5">
+      <div class="col-sm-8 col-md-10">
+        <form action="#">
+          <input class="form-control" type="text" placeholder="  Seach location apartment..." />
+          {{-- <i class="fa fa-search" aria-hidden="true"></i> --}}
+        </form>
+      </div>
+      <div class="col-sm-2 col-md-1">
+        <span class="btn btn-outline-primary">Filter</span>
+      </div>
+      <div class="col-sm-2 col-md-1">
+        <span class="dropdown">
+          <button class="btn btn-outline-primary dropdown-toggle position-relative" type="button" id="dropdownMenuButton"
+            data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            Sort
+          </button>
+          <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+            <a class="dropdown-item" href="#">Action</a>
+            <a class="dropdown-item" href="#">Another action</a>
+            <a class="dropdown-item" href="#">Something else here</a>
+          </div>
+        </span>
+      </div>
+    </div>
+  </div>
+</section>
---
+++ resources/views/partials/student/share-hostel-search-bar.blade.php
@@ -0,0 +1,19 @@
+{{-- Search bar --}}
+<section class="mb-5">
+  <div class="container">
+    <div class="row pt-5">
+      <div class="col-sm-8 col-md-10">
+        <form action="#">
+          <input class="form-control" type="text" placeholder="Seach hostel" />
+          {{-- <i class="fa fa-search" aria-hidden="true"></i> --}}
+        </form>
+      </div>
+      <div class="col-sm-4 col-md-2">
+        <span class="btn btn-outline-primary">
+            <i class="fa fa-plus-circle"></i>
+            Share a hostel
+        </span>
+      </div>
+    </div>
+  </div>
+</section>
