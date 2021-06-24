33fd36faf9c43c9e17c83e034191f80b758f3b05
Favour Okunowo <toluwaniokunowo@gmail.com>
2021-06-24 04:14:53 +0100

    add favorite function for students

---
+++ app/Http/Controllers/Student/FavoriteHostelController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers\Student;
+
+use App\Http\Controllers\Controller;
+use App\Models\Hostel;
+use App\Models\Student;
+use Illuminate\Http\Request;
+
+class FavoriteHostelController extends Controller
+{
+    public function store($id)
+    {
+        $user = Student::find(auth('student')->id());
+        $hostel = Hostel::findOrFail($id);
+
+
+        $user->favorite($hostel);
+        $user->unfavorite($hostel);
+        $user->toggleFavorite($hostel);
+        $user->getFavoriteItems(Hostel::class);
+
+        $user->hasFavorited($hostel);
+        $hostel->hasBeenFavoritedBy($user);
+
+        return back()->with('success', 'Hostel added to favorites');
+    }
+}
