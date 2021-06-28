118dd2d6a79dd282e8aa513608e9b980f29ee58f
Favour Okunowo <toluwaniokunowo@gmail.com>
2021-06-28 13:43:41 +0100

    Correct breadcrumb names

--- routes/breadcrumbs.php
+++ routes/breadcrumbs.php
@@ -3,16 +3,16 @@
 use Diglactic\Breadcrumbs\Breadcrumbs;
 
 Breadcrumbs::for('home', function ($trail) {
-    $trail->push('Student Home', route('student.index'));
+    $trail->push('Settings');
 });
 
-// Student Home > Account Settings
+// Student Settings -> Account
 Breadcrumbs::for('account', function ($trail) {
     $trail->parent('home');
     $trail->push('Account', route('student.settings.account'));
 });
 
-// Student Home > Profile Settings
+// Student Settings -> Profile
 Breadcrumbs::for('profile', function ($trail) {
     $trail->parent('home');
     $trail->push('Profile', route('student.settings.profile'));
