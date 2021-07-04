8d7004f5ee51dc6a8dd9f09f7a6c6084de932859
Favour Okunowo <toluwaniokunowo@gmail.com>
2021-07-04 23:06:30 +0100

    Update 2021_07_01_225633_create_preferences_table.php

--- database/migrations/2021_07_01_225633_create_preferences_table.php
+++ database/migrations/2021_07_01_225633_create_preferences_table.php
@@ -15,13 +15,21 @@ class CreatePreferencesTable extends Migration
     {
         Schema::create('preferences', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('student_id')->index();
             $table->string('location');
             $table->string('property_type');
             $table->integer('no_of_rooms');
             $table->string('rent_period');
             $table->timestamps();
+
+            $table->foreign('student_id')
+                ->references('id')
+                ->on('students')
+                ->onDelete('cascade');
         });
     }
+
+            
 
     /**
      * Reverse the migrations.
