7e08fcbe9988ab67f89d2a3c29faf29ccce2a1a7
Favour Okunowo <toluwaniokunowo@gmail.com>
2021-07-04 23:40:19 +0100

    Create AgentSeeder.php

---
+++ database/seeders/AgentSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Agent;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class AgentSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Agent::create([
+            'name' => 'Sand Castle',
+            'email' => 'sand@gmail.com',
+            'phone' => '1234567',
+            'password' => Hash::make('password'),
+            'image' => 'default.jpg',
+        ]);
+    }
+}
