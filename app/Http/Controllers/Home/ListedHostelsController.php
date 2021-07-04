efcca6877653806b9d62927d2d1cf135399e0d8a
Favour Okunowo <toluwaniokunowo@gmail.com>
2021-07-04 23:05:58 +0100

    populate filter modal

--- app/Http/Controllers/Home/ListedHostelsController.php
+++ app/Http/Controllers/Home/ListedHostelsController.php
@@ -4,8 +4,10 @@ namespace App\Http\Controllers\Home;
 
 use App\Http\Controllers\Controller;
 use App\Models\Agent;
+use App\Models\City;
 use App\Models\Favorite;
 use App\Models\Hostel;
+use App\Models\Property;
 use Illuminate\Http\Request;
 
 class ListedHostelsController extends Controller
@@ -16,7 +18,10 @@ class ListedHostelsController extends Controller
             ->available()
             ->Paginate(16);
 
-        return view('frontend.hostels', compact('hostels'));
+        $location = City::get();
+        $property = Property::get();
+
+        return view('frontend.hostels', compact('hostels', 'location', 'property'));
     }
 
     public function show(Hostel $hostel)
