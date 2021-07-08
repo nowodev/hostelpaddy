54965e71906dcca1c3b932394ecc4040b3bae47d
Favour Okunowo <toluwaniokunowo@gmail.com>
2021-07-08 18:23:30 +0100

    rename controller

---
+++ app/Http/Controllers/Home/FilterController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers\Home;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class FilterController extends Controller
+{
+    public function index(Request $request)
+    {
+        //
+    }
+}
