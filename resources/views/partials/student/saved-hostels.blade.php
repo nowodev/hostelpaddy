2d553df291916be295200f395be79cfc95a61e50
Favour Okunowo <toluwaniokunowo@gmail.com>
2021-07-08 19:47:57 +0100

    show 'no records' when empty

--- resources/views/partials/student/saved-hostels.blade.php
+++ resources/views/partials/student/saved-hostels.blade.php
@@ -5,6 +5,10 @@
   @include('partials.student.search-bar')
 
   <div class="row">
+    {{-- @if ($favoriteHostels == '')
+        Emptiness
+    @endif --}}
+
     @foreach ($favoriteHostels as $hostel)
       <div class="col-6 col-md-4 col-lg-3 mb-3">
         <div class="card">
