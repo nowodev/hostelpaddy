2b25b9195088dceb4dbe514d96b3d103b3e14ba6
Favour Okunowo <toluwaniokunowo@gmail.com>
2021-07-10 15:28:05 +0100

    use 'View Routes'

--- routes/custom/main.php
+++ routes/custom/main.php
@@ -4,43 +4,22 @@ use App\Http\Controllers\Home\ListedHostelsController;
 use App\Http\Controllers\Home\PreferenceController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/about', function () {
-    return view('frontend.about');
-})->name('about');
-
+// Dynamic  Page Routes
 Route::get('/hostels', [ListedHostelsController::class, 'index'])
     ->name('hostels');
 
 Route::post('/hostels', [PreferenceController::class])
     ->name('filter.preference');
-    
+
 Route::get('/hostels/{availableHostel:slug}', [ListedHostelsController::class, 'show'])
     ->name('info');
 
-Route::get('/house', function () {
-    return view('frontend.house');
-})->name('house');
-
-Route::get('/help', function () {
-    return view('frontend.help');
-})->name('help');
-
-Route::get('/onboarding', function () {
-    return view('frontend.onboarding');
-})->name('onboarding');
-
-Route::get('/contact', function () {
-    return view('frontend.contact');
-})->name('contact');
-
-Route::get('/faqs', function () {
-    return view('frontend.faqs');
-})->name('faqs');
-
-Route::get('/tos', function () {
-    return view('frontend.tos');
-})->name('tos');
-
-Route::get('/privacy', function () {
-    return view('frontend.privacy');
-})->name('privacy');
+// Static Page Routes
+Route::view('/about', 'frontend.about')->name('about');
+Route::view('/house', 'frontend.house')->name('house');
+Route::view('/help', 'frontend.help')->name('help');
+Route::view('/onboarding', 'frontend.onboarding')->name('onboarding');
+Route::view('/contact', 'frontend.contact')->name('contact');
+Route::view('/faqs', 'frontend.faqs')->name('faqs');
+Route::view('/tos', 'frontend.tos')->name('tos');
+Route::view('/privacy', 'frontend.privacy')->name('privacy');
