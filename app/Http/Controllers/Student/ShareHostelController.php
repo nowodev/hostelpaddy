d21de935f534de183d7313e443de05967e09b9d5
Favour Okunowo <toluwaniokunowo@gmail.com>
2021-07-18 11:20:14 +0100

    link create form

--- app/Http/Controllers/Student/ShareHostelController.php
+++ app/Http/Controllers/Student/ShareHostelController.php
@@ -3,18 +3,19 @@
 namespace App\Http\Controllers\Student;
 
 use App\Http\Controllers\Controller;
+use App\Models\Hostel;
 use Illuminate\Http\Request;
 
 class ShareHostelController extends Controller
 {
     public function index()
     {
-        //
+        return view('students.hostel_mate');
     }
 
     public function create()
     {
-        //
+        return view('students.share-hostels.create');
     }
 
     public function store(Request $request)
@@ -22,22 +23,22 @@ class ShareHostelController extends Controller
         //
     }
 
-    public function show($id)
+    public function show(Hostel $hostel)
     {
         //
     }
 
-    public function edit($id)
+    public function edit(Hostel $hostel)
     {
         //
     }
 
-    public function update(Request $request, $id)
+    public function update(Request $request, Hostel $hostel)
     {
         //
     }
 
-    public function destroy($id)
+    public function destroy(Hostel $hostel)
     {
         //
     }
