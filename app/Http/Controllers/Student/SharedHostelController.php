4ed7e156084b987982a28c97cc1e41c7addd7f7e
Favour Okunowo <toluwaniokunowo@gmail.com>
2021-07-18 23:55:44 +0100

    declare relationships

--- app/Http/Controllers/Student/SharedHostelController.php
+++ app/Http/Controllers/Student/SharedHostelController.php
@@ -1,7 +1,8 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\Student;
 
+use App\Http\Controllers\Controller;
 use App\Models\SharedHostel;
 use Illuminate\Http\Request;
 
--- app/Models/SharedHostel.php
+++ app/Models/SharedHostel.php
@@ -8,4 +8,25 @@ use Illuminate\Database\Eloquent\Model;
 class SharedHostel extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'phone_number', 'level', 'institution',
+        'gender', 'amenities', 'utilities',
+        'choice', 'photos', 'video',
+    ];
+
+    public function students()
+    {
+        return $this->belongsTo(Student::class);
+    }
+
+    public function amenities()
+    {
+        return $this->belongsToMany(Amenity::class);
+    }
+
+    public function utilities()
+    {
+        return $this->belongsToMany(Utility::class);
+    }
 }
