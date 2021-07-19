c9ded865a6682298aa6c045295895847afda04ba
Favour Okunowo <toluwaniokunowo@gmail.com>
2021-07-19 05:02:34 +0100

    show shared hostels

--- resources/views/partials/student/shared-hostels.blade.php
+++ resources/views/partials/student/shared-hostels.blade.php
@@ -1,9 +1,8 @@
 {{-- Hostel Details --}}
 
-@for ($q = 0; $q <= 2; $q++)
 <div class="row mb-3">
-  @for ($i = 0; $i <= 2; $i++)
-    <div class="col">
+  @foreach ($sharedHostel as $hostel)
+    <div class="col-4">
       <div class="card">
         <div class="row no-gutters">
           <img class="card-pill-img card-img-top img-fluid" src="{{ asset('main/img/hostel.png') }}" alt="">
@@ -13,15 +12,20 @@
           <div class="col">
             <span class="card-body fs-6">
               <p class="card-text">1 Room Duplex Hostel</p>
-              <p>Host: Ademola Tunji</p>
-              <p>Bedroom, wardrobe, bathroom...</p>
-              <p>Surlere Lagos.</p>
+              <p>Host:
+                    {{ dd($hostel->students) }}
+              </p>
+              <p>
+                @foreach ($hostel->amenities as $utility)
+                  {{ $utility->name }}
+                @endforeach
+              </p>
+              <p>{{ $hostel->state }} Surlere Lagos.</p>
               <p class="">#5,000/monthly</p>
             </span>
           </div>
         </div>
       </div>
     </div>
-  @endfor
+  @endforeach
 </div>
-@endfor
