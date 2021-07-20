43eaaf0e15639b1ab97a65d7c9e34251f1e0146c
Favour Okunowo <toluwaniokunowo@gmail.com>
2021-07-20 03:11:12 +0100

    define filters

---
+++ app/EloquentFilters/Hostel/RentPeriodFilter.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\EloquentFilters\Hostel;
+
+use Fouladgar\EloquentBuilder\Support\Foundation\Contracts\Filter;
+use Illuminate\Database\Eloquent\Builder;
+
+class RentPeriodFilter extends Filter
+{
+    /**
+     * Apply the condition to the query.
+     *
+     * @param Builder $builder
+     * @param mixed $value
+     *
+     * @return Builder
+     */
+    public function apply(Builder $builder, $value): Builder
+    {
+         return $builder->where('period', '=', $value);
+    }
+}
