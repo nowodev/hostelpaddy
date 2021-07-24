f8d2429216f73a6bf9c46bbfe1340a5d4d231a85
Favour Okunowo <toluwaniokunowo@gmail.com>
2021-07-24 17:37:39 +0100

    ⚙️ change method of showing message when fields are empty

--- resources/views/partials/student/saved.blade.php
+++ resources/views/partials/student/saved.blade.php
@@ -1,29 +1,23 @@
 {{-- Saved Hostel Details --}}
 
 <div class="row mb-3">
-  @if($favoriteSharedHostels->isEmpty())
-    <div class="col">
-      <h5 class="font-weight-bold">You dont have any favorite shared hostels.</h5>
-    </div>
-  @else
+  @forelse ($favoriteSharedHostels as $hostel)
+    <div class="col-4 mb-3">
+      <div class="card">
+        <div class="row">
+          <img class="card-pill-img card-img-top img-fluid" src="{{ asset('main/img/hostel.png') }}" alt="">
+          <div class="card-img-overlay">
+            <form action="{{ route('student.sharedUnfave', [$hostel]) }}" method="POST"
+                  class="inline-flex clearfix float-left">
+              @csrf
+              @method('DELETE')
+              <button class="btn mt-n3 ml-n3">
+                <img src="{{ asset('main/img/loved.png') }}" alt="Card image cap">
+              </button>
+            </form>
+          </div>
 
-    @foreach ($favoriteSharedHostels as $hostel)
-      <div class="col-4 mb-3">
-        <div class="card">
-          <div class="row">
-            <img class="card-pill-img card-img-top img-fluid" src="{{ asset('main/img/hostel.png') }}" alt="">
-            <div class="card-img-overlay">
-              <form action="{{ route('student.sharedUnfave', [$hostel]) }}" method="POST"
-                    class="inline-flex clearfix float-left">
-                @csrf
-                @method('DELETE')
-                <button class="btn mt-n3 ml-n3">
-                  <img src="{{ asset('main/img/loved.png') }}" alt="Card image cap">
-                </button>
-              </form>
-            </div>
-
-            <div class="col">
+          <div class="col">
             <span class="card-body fs-6">
               <p class="card-text">1 Room Duplex Hostel</p>
 {{--              <p class="card-text">{{ $hostel->description }}</p>--}}
@@ -42,10 +36,14 @@
 {{--              <p>{{ $hostel->state }}</p>--}}
               <p class="">#5,000/monthly</p>
             </span>
-            </div>
           </div>
         </div>
       </div>
-    @endforeach
-  @endif
+    </div>
+
+  @empty
+    <div class="col">
+      <h5 class="font-weight-bold">You dont have any favorite shared hostels.</h5>
+    </div>
+  @endforelse
 </div>
--- resources/views/partials/student/shared-hostels.blade.php
+++ resources/views/partials/student/shared-hostels.blade.php
@@ -1,21 +1,15 @@
 {{-- Hostel Details --}}
 
 <div class="row mb-3">
-  @if($sharedHostel->isEmpty())
-    <div class="col">
-      <h5 class="font-weight-bold">There are no available shared hostels currently. Please check back later.</h5>
-    </div>
-  @else
+  @forelse ($sharedHostel as $hostel)
+    <div class="col-4 mb-3">
+      <div class="card">
+        <div class="row">
+          <img class="card-pill-img card-img-top img-fluid" src="{{ asset('main/img/hostel.png') }}" alt="">
 
-    @foreach ($sharedHostel as $hostel)
-      <div class="col-4 mb-3">
-        <div class="card">
-          <div class="row">
-            <img class="card-pill-img card-img-top img-fluid" src="{{ asset('main/img/hostel.png') }}" alt="">
+          @include('partials.student.favorite')
 
-            @include('partials.student.favorite')
-
-            <div class="col">
+          <div class="col">
             <span class="card-body fs-6">
               <p class="card-text">1 Room Duplex Hostel</p>
 {{--              <p class="card-text">{{ $hostel->description }}</p>--}}
@@ -36,10 +30,14 @@
 {{--              <p>{{ $hostel->state }}</p>--}}
               <p class="">#5,000/monthly</p>
             </span>
-            </div>
           </div>
         </div>
       </div>
-    @endforeach
-  @endif
+    </div>
+  @empty
+
+    <div class="col">
+      <h5 class="font-weight-bold">There are no available shared hostels currently. Please check back later.</h5>
+    </div>
+  @endforelse
 </div>
