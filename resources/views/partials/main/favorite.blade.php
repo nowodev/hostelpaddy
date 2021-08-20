b7d656e2627cad141af493d275962ac2eaa1ede7
Favour Okunowo <toluwaniokunowo@gmail.com>
2021-08-20 04:14:35 +0100

    🎨 restyle favorite icon & position

--- resources/views/partials/main/favorite.blade.php
+++ resources/views/partials/main/favorite.blade.php
@@ -1,25 +1,21 @@
 @auth('agent')
 @else
   @auth('student')
-    <div class="card-img-overlay">
+    <div class="position-absolute love">
       @if (auth('student')->user()->isFavorited($hostel))
-        <form action="{{ route('student.unfave', [$hostel]) }}" method="POST" class="inline-flex clearfix float-right">
+        <form action="{{ route('student.unfave', [$hostel]) }}" method="POST">
           @csrf
           @method('DELETE')
           <button>
-            <button class="btn">
-              <img src="{{ asset('main/img/loved.png') }}" alt="Card image cap">
-            </button>
+            <img src="{{ asset('main/img/loved.png') }}" alt="Card image cap">
           </button>
         </form>
       @else
-        <form action="{{ route('student.fave', [$hostel]) }}" method="POST" class="inline-flex clearfix float-right">
+        <form action="{{ route('student.fave', [$hostel]) }}" method="POST">
           @csrf
           @method('PUT')
           <button>
-            <button class="btn">
-              <img src="{{ asset('main/img/love.png') }}" alt="Card image cap">
-            </button>
+            <img src="{{ asset('main/img/love.png') }}" alt="Card image cap">
           </button>
         </form>
       @endif
@@ -27,14 +23,12 @@
   @endauth
 
   @guest('student')
-    <div class="card-img-overlay">
-      <form action="{{ route('student.fave', [$hostel]) }}" method="POST" class="inline-flex clearfix float-right">
+    <div class="position-absolute love">
+      <form action="{{ route('student.fave', [$hostel]) }}" method="POST">
         @csrf
         @method('PUT')
         <button>
-          <button class="btn">
-            <img src="{{ asset('main/img/love.png') }}" alt="Card image cap">
-          </button>
+          <img src="{{ asset('main/img/love.png') }}" alt="Card image cap">
         </button>
       </form>
     </div>
--- resources/views/partials/main/similar-hostel-favorite.blade.php
+++ resources/views/partials/main/similar-hostel-favorite.blade.php
@@ -1,22 +1,16 @@
-<div class="card-img-overlay">
-  <form action="{{ route('student.fave', [$others]) }}" method="POST" class="inline-flex float-right">
+<div class="position-absolute love">
+  <form action="{{ route('student.fave', [$others]) }}" method="POST">
     @csrf
     @method('PUT')
     <button>
       @auth('student')
         @if (auth('student')->user()->hasFavorited($others))
-          <button class="btn">
-            <img src="{{ asset('main/img/loved.png') }}" alt="Card image cap">
-          </button>
+          <img src="{{ asset('main/img/loved.png') }}" alt="Card image cap">
         @else
-          <button class="btn">
-            <img src="{{ asset('main/img/love.png') }}" alt="Card image cap">
-          </button>
+          <img src="{{ asset('main/img/love.png') }}" alt="Card image cap">
         @endif
       @else
-        <button class="btn">
-          <img src="{{ asset('main/img/love.png') }}" alt="Card image cap">
-        </button>
+        <img src="{{ asset('main/img/love.png') }}" alt="Card image cap">
       @endauth
     </button>
   </form>
