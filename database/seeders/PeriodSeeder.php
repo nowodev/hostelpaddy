53f77a80538f502ce47524c5a49195c72816f904
Favour Okunowo <toluwaniokunowo@gmail.com>
2021-08-23 23:58:55 +0100

    💡♲ add & fetch periods to db

---
+++ database/seeders/PeriodSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class PeriodSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $periods = ['Monthly', 'Quarterly', 'Yearly'];
+    
+        foreach($periods as $period) {
+            DB::table('periods')->insert(['name' => $period]);
+        }
+    }
+}
