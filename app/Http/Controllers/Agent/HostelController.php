4e7e043aec79380ed0f14137de67f20f726c7c62
Favour Okunowo <toluwaniokunowo@gmail.com>
2021-08-26 20:43:53 +0100

    ♺ resize image before inserting into db

--- app/Http/Controllers/Agent/HostelController.php
+++ app/Http/Controllers/Agent/HostelController.php
@@ -22,14 +22,14 @@ class HostelController extends Controller
     {
         $this->authorizeResource(Hostel::class, 'hostel');
     }
-    
+
     public function index()
     {
         $hostels = Hostel::agent()->where('available', 1)->get();
-        
+
         return view('agents.listing', compact('hostels'));
     }
-    
+
     public function create(Hostel $hostel)
     {
         $states = State::get();
@@ -38,19 +38,19 @@ class HostelController extends Controller
         $utilities = Utility::get();
         $rules = Rule::get();
         $periods = Period::get();
-        
+
         return view(
             'agents.hostels.create',
             compact('hostel', 'states', 'properties', 'amenities', 'utilities', 'rules', 'periods')
         );
     }
-    
+
     public function fetchCity(Request $request)
     {
         $data['cities'] = City::where("state_id", $request->state_id)->get(["name", "id"]);
         return response()->json($data);
     }
-    
+
     public function store(HostelRequest $request)
     {
         // ? Add hostel to table, and populate other tables(hostel_utility, amenity_hostel, hostel_rule) with data
@@ -59,28 +59,27 @@ class HostelController extends Controller
             if ($request->hasFile('coverImage')) {
                 $this->_uploadCoverImage($request, $hostel);
             }
-            
+
             if ($request->hasFile('images')) {
                 $this->_uploadImages($request, $hostel);
             }
-            
+
             $hostel->amenities()->sync($request->amenities);
             $hostel->utilities()->sync($request->utilities);
             $hostel->rules()->sync($request->rules);
-            
+
             notify()->preset('hostel-added');
             return redirect()->route('agent.hostels.index');
         }
-        
+
         return redirect()->back()
             ->with('error', 'A problem occurred');
     }
-    
+
     public function show($id)
     {
-    
     }
-    
+
     public function edit(Hostel $hostel)
     {
         $amenities = Amenity::get();
@@ -90,39 +89,39 @@ class HostelController extends Controller
         $states = State::get();
         $cities = City::get();
         $periods = Period::get();
-        
+
         return view(
             'agents.hostels.edit',
             compact('hostel', 'amenities', 'utilities', 'rules', 'properties', 'states', 'cities', 'periods')
         );
     }
-    
+
     public function update(HostelRequest $request, Hostel $hostel)
     {
         $hostel->update($request->validated());
         if ($request->hasFile('coverImage')) {
             $this->_uploadCoverImage($request, $hostel);
         }
-        
+
         if ($request->hasFile('images')) {
             $this->_uploadImages($request, $hostel);
         }
-        
+
         $hostel->amenities()->sync($request->amenities);
         $hostel->utilities()->sync($request->utilities);
         $hostel->rules()->sync($request->rules);
-        
+
         notify()->preset('hostel-updated');
         return redirect()->route('agent.hostels.index');
     }
 
-//    public function destroy(Hostel $hostel)
-//    {
-//        $hostel->delete();
-//        notify()->preset('hostel-deleted');
-//        return redirect()->route('agent.hostels.index');
-//    }
-    
+    //    public function destroy(Hostel $hostel)
+    //    {
+    //        $hostel->delete();
+    //        notify()->preset('hostel-deleted');
+    //        return redirect()->route('agent.hostels.index');
+    //    }
+
     public function destroy(Hostel $hostel)
     {
         $hostel->amenities()->detach($hostel->amenities);
@@ -132,28 +131,28 @@ class HostelController extends Controller
         notify()->preset('hostel-deleted');
         return redirect()->route('agent.hostels.index');
     }
-    
+
     private function _uploadCoverImage($request, $hostel)
     {
         $image = $request->file('coverImage');
         $name = $request->hostel_name;
         $filename = 'HP_H_' . $name . '.' . $image->getClientOriginalExtension();
         Image::make($image)->save(storage_path('app/public/hostels/' . $filename));
-        
+
         $hostel->coverImage = $filename;
         $hostel->save();
     }
-    
+
     private function _uploadImages($request, $hostel)
     {
         $images = $request->file('images');
         $name = $request->hostel_name;
         foreach ($images as $image) {
             $filename = 'HP_H_' . $name . Str::random(3) . '.' . $image->getClientOriginalExtension();
-            // Image::make($image)->resize(225, 100)
-            // Do not resize the image
-            Image::make($image)->save(storage_path('app/public/hostels/' . $filename));
-//            $hostel->image = $filename;
+            Image::make($image)->resize(300, null, function ($constraint) {
+                $constraint->aspectRatio();
+            })->save(storage_path('app/public/hostels/' . $filename));
+            //            $hostel->image = $filename;
             $hostel->images()->create(['image' => $filename]);
             $hostel->save();
         }
