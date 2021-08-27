4662859ec19015917ec0186f532aeb87af77c817
Favour Okunowo <toluwaniokunowo@gmail.com>
2021-08-27 02:26:53 +0100

    🧪 use bootstrap 5 for admin panel

--- resources/views/layouts/admin/app.blade.php
+++ resources/views/layouts/admin/app.blade.php
@@ -1,42 +1,28 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>@yield('title', 'Hostel Paddy')</title>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>@yield('title', 'Hostel Paddy')</title>
 
-        <!-- Fonts -->
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
+  <!-- Fonts -->
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
 
-        <!-- Styles -->
-        <link rel="stylesheet" href="{{ asset('main/tailwind-assets/css/tailwind.output.css') }}">
+  <!-- Styles -->
+  <link rel="stylesheet" href="{{ asset('main/vendor/bootstrap-5.1.0-dist/css/bootstrap.min.css') }}">
+</head>
 
-        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+<body>
+  @include('layouts.admin.navigation')
 
-        <script src="{{ asset('main/tailwind-assets/js/alpine.min.js') }}"></script>
-        <script src="{{ asset('main/tailwind-assets/js/init-alpine.js') }}"></script>
-        <script src="{{ asset('main/tailwind-assets/js/focus-trap.js') }}" defer></script>
+  <div class="container">
+    @yield('content')
+  </div>
 
-        <!-- Scripts -->
-        <script src="{{ asset('js/app.js') }}" defer></script>
-    </head>
-    <body class="font-sans antialiased">
-        <div class="min-h-screen bg-gray-100">
-            @include('layouts.admin.navigation')
+  <script src="{{ asset('main/vendor/jquery/jquery-3.4.1.min.js') }}"></script>
+  <script src="{{ asset('main/vendor/bootstrap-5.1.0-dist/js/bootstrap.bundle.min.js') }}"></script>
+  <script src="{{ asset('main/vendor/fontawesome/d7644e187f.js') }}"></script>
+</body>
 
-            <!-- Page Heading -->
-            <header class="bg-white shadow">
-                <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                    {{ $header }}
-                </div>
-            </header>
-
-            <!-- Page Content -->
-            <main>
-                {{ $slot }}
-            </main>
-        </div>
-    </body>
 </html>
