f279579fa969751bd571e527aef32f615ad1ef7a
Favour Okunowo <toluwaniokunowo@gmail.com>
2021-08-27 20:35:45 +0100

    🔧 data inserted in wrong table

--- resources/views/admin/features/utility.blade.php
+++ resources/views/admin/features/utility.blade.php
@@ -61,7 +61,7 @@
           <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
         </div>
         <div class="modal-body">
-          <form action="{{ route('admin.amenities.store') }}" method="POST">
+          <form action="{{ route('admin.utilities.store') }}" method="POST">
             @csrf
             <div class="mb-3">
               <label for="utilityName" class="col-form-label">Name:</label>
