30f57eb0ec52b718ec717a37e44b139914bee71f
Favour Okunowo <toluwaniokunowo@gmail.com>
2021-08-27 15:28:04 +0100

    ➕❌ add & delete rules from table

---
+++ app/Http/Controllers/Admin/RuleController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use App\Models\Rule;
+use Illuminate\Http\Request;
+
+class RuleController extends Controller
+{
+    public function index()
+    {
+        $rules = Rule::Paginate(5, ['*'], 'rules');
+
+        return view('admin.features.rules', compact('rules'));
+    }
+
+    public function store(Request $request)
+    {
+        $credentials = $request->validate(['name' => 'required|string']);
+        Rule::create($credentials);
+
+        notify()->preset('feature-added');
+        return redirect()->back();
+    }
+
+    public function show(Rule $rule)
+    {
+        //
+    }
+
+    public function edit(Rule $rule)
+    {
+        //
+    }
+
+    public function update(Request $request, Rule $rule)
+    {
+        //
+    }
+
+    public function destroy(Rule $rule)
+    {
+        $rule->delete();
+
+        notify()->preset('feature-deleted');
+        return redirect()->back();
+    }
+}
